bddce3fb602d0d8b7a01ff5c31ffbdb9aeebc720
Haniye <esmaeilihaniye@yahoo.com>
2023-11-12 02:24:36 -0800

    Fix a bug

--- Assets/Scripts/MechanicData.cs
+++ Assets/Scripts/MechanicData.cs
@@ -1,17 +1,13 @@
 using System.Threading.Tasks;
 using UnityEngine;
-public abstract class MechanicData:ScriptableObject
+public abstract class MechanicData : ScriptableObject
 {
     public Sprite MechanicSprite;
     [SerializeField] protected Cube _cubePrefab;
-    public abstract Task<bool> Move( Player player);
-    private Cube[] cubes = null;
-    protected bool IsValidPos(Vector3 pos) 
+    public abstract Task<bool> Move(Player player);
+    protected bool IsValidPos(Vector3 pos)
     {
-        if (null == cubes)
-        {
-            cubes = FindObjectsOfType<Cube>();
-        }
+        Cube[] cubes = FindObjectsOfType<Cube>();
 
         for (int i = 0; i < cubes.Length; i++)
         {
